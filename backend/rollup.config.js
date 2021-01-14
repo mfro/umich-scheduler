10208d867f0aa4906711445d13b16a90f97b9821
Max Froehlich <max@mfro.me>
2021-01-13 20:08:31 -0500

    Clean up dependencies and build

--- backend/rollup.config.js
+++ backend/rollup.config.js
@@ -1,17 +1,42 @@
+import json from '@rollup/plugin-json';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+
 module.exports = {
     input: 'src/main.js',
     output: {
         file: 'main.js',
         format: 'cjs',
-        sourcemap: true,
     },
+    plugins: [
+        json(),
+        nodeResolve(),
+        commonjs(),
+    ],
     external: [
         'path',
         'https',
         'readline',
         'perf_hooks',
-        
-        'express',
-        'fs-extra',
     ],
 };
+
+// module.exports = {
+//   input: 'src/main.ts',
+//   output: {
+//     file: 'main.js',
+//     format: 'cjs',
+//   },
+//   context: 'this',
+//   plugins: [
+//     json(),
+//     typescript(),
+//     nodeResolve(),
+//     commonjs({
+//       ignore: ['bufferutil', 'utf-8-validate'],
+//     }),
+//   ],
+//   external: [
+//     'url',
+//   ],
+// };
